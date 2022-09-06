eb0b0795aeb8c06b4c8f4f5d05a1b981f81a3e86
bishwas-py <bishwasbh@gmail.com>
2022-09-06 18:46:56 +0545

    Google Analytics Script added

--- layer/Layout.jsx
+++ layer/Layout.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import Head from "next/head";
+import Script from "next/script";
 
 function Layout({className='', title="CPS Test", children}) {
     return (
         <main className={className}>
+            <div>
+                <Script strategy={"lazyOnload"} dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                    
+                        gtag('config', 'G-MQMPTMDET6');
+                    `
+                }}/>
+            </div>
             <Head>
                 <title>{`${title} | Speed Clicking`}</title>
                 <meta name="description" content="A speed clicking test to see how many clicks per second you can do. Speed Clicking helps you to increase your CPS [click per second] rates."/>

89af77e5c7a59399aebda458ca93e8af2a79d43a
bishwas-py <bishwasbh@gmail.com>
2022-09-06 15:02:23 +0545

    Responsive Restart button

--- components/section/play/ResultBox.jsx
+++ components/section/play/ResultBox.jsx
@@ -8,23 +8,23 @@ function ResultBox({count, countsPerSecond, rank, defaultTimeLeft, timeLeft, sta
         <>
             {!timeLeft && <div
                 className={"result-box"}>
-                <div className={"text-center flex flex-col justify-center min-h-0 max-h-full"}>
+                <div className={"result-box-container"}>
                     <div>
-                        <h2 className={"text-5xl md:text-6xl font-bold mb-3 md:mb-7"}>{rank.name}</h2>
-                        <h3 className={"text-lg md:text-xl"}>Your CPS result
+                        <h2 className={"result-heading"}>{rank.name}</h2>
+                        <h3 className={"md:text-lg md:text-xl"}>Your CPS result
                             is {countsPerSecond} ({count} Clicks
                             in {defaultTimeLeft} Seconds)</h3>
                     </div>
                     <img src={rank.icon} alt={'Your Avatar'}
-                         className={"mx-auto h-full mb-4 min-h-0 max-h-[240px]"} draggable={"false"}/>
+                         className={"result-image"} draggable={"false"}/>
                     <div>
                         <h3 className={"text-sm font-light max-w-sm px-2 mx-auto"}>{rank.description}</h3>
                     </div>
-                    <div className={"flex flex-col w-72 mx-auto gap-2"}>
+                    <div className={"flex flex-col mx-auto gap-2"}>
                         <button className={"group mt-4"} onClick={start}>
                             <BigButton>
-                                <div className={"px-7 gap-3 flex justify-between w-full my-auto relative"}>
-                                    <div className={"my-auto w-10"}>
+                                <div className={"px-7 gap-3 flex my-auto relative text-sm sm:text-xl md:text-2xl"}>
+                                    <div className={"my-auto w-6 sm:w-7 md:w-10"}>
                                         <FontAwesomeIcon icon={faRedo}
                                                          className={"w-full group-hover:rotate-90 duration-300"}/>
                                     </div>
